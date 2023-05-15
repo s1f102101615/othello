07400b52111d633da75dcbd6c3932a38798957b9
s1f102101615 <s1f102101615@iniad.org>
2023-05-16 03:54:12 +0900

    ゲーム性完成、予測

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,43 @@ const Home = () => {
   const onClick = (x: number, y: number) => {
     console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-
+    //これから予測
+    for (let tate=0 ; tate<7;tate++) {
+      for (let yoko = 0 ; yoko<7;yoko++)  {
+        if (board[tate][yoko] === 0) {
+          for (let t = 0; t < directions.length; t++) {
+            console.log(Math.max(yoko + directions[t][1], 0), Math.max(tate + directions[t][0], 0));
+            if (
+              board[Math.min(Math.max(tate + directions[t][0], 0), 7)][
+                Math.max(yoko + directions[t][1], 0)
+              ] !== undefined &&
+              board[Math.min(Math.max(tate + directions[t][0], 0), 7)][
+                Math.max(yoko + directions[t][1], 0)
+              ] !== 0 &&
+              board[Math.min(Math.max(tate + directions[t][0], 0), 7)][
+                Math.max(yoko + directions[t][1], 0)
+              ] !== turnColor
+            ) {
+              if (
+                board[Math.min(Math.max(tate + directions[t][0], 0), 7)][
+                  Math.max(yoko + directions[t][1], 0)
+                ] !== turnColor
+              ) {
+                for (let p = 2; p < 8; p++) {
+                  if (
+                    board[tate + directions[t][0] * p] === undefined ||
+                    board[yoko + directions[t][1] * p] === undefined ||
+                    board[tate + directions[t][0] * p][yoko + directions[t][1] * p] === 0
+                  ) {
+                    break;
+                  }
+                  if (board[tate + directions[t][0] * p][yoko + directions[t][1] * p] === turnColor) {
+                    newBoard[tate][yoko] = 3
+                  }
+                }
+              }}
+      }
+    }
     //周囲一マスに違う色の駒が無いと置けない 一行目は押した場所に駒があるかどうか
     if (board[y][x] === 0) {
       for (let t = 0; t < directions.length; t++) {
@@ -44,8 +80,34 @@ const Home = () => {
             Math.max(x + directions[t][1], 0)
           ] !== turnColor
         ) {
-          newBoard[y][x] = turnColor;
-          setTurnColor(3 - turnColor);
+          if (
+            board[Math.min(Math.max(y + directions[t][0], 0), 7)][
+              Math.max(x + directions[t][1], 0)
+            ] !== turnColor
+          ) {
+            let turn = 2;
+            for (let p = 2; p < 8; p++) {
+              if (
+                board[y + directions[t][0] * p] === undefined ||
+                board[x + directions[t][1] * p] === undefined ||
+                board[y + directions[t][0] * p][x + directions[t][1] * p] === 0
+              ) {
+                break;
+              }
+              if (board[y + directions[t][0] * p][x + directions[t][1] * p] === turnColor) {
+                console.log('aa');
+                newBoard[y][x] = turnColor;
+                for (let now = 1; now < turn; now++) {
+                  console.log(now);
+                  newBoard[y + directions[t][0] * (p - now)][x + directions[t][1] * (p - now)] =
+                    turnColor;
+                }
+
+                setTurnColor(3 - turnColor);
+              }
+              turn++;
+            }
+          }
         }
       }
       //
@@ -66,7 +128,9 @@ const Home = () => {
               {color !== 0 && (
                 <div
                   className={styles.stone}
-                  style={{ background: color === 1 ? '#000' : '#fff' }}
+                  style={{ background: color === 1 ? '#000' : (color === 3 ? '#ccff00' : '#fff') }}
+
+
                 />
               )}
             </div>
