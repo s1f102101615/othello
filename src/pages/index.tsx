64e8ba155459072ac0ba4dfe83b3275c26cdeb0c
s1f102101615 <s1f102101615@iniad.org>
2023-05-16 09:55:21 +0900

    予測作成中

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,43 +28,7 @@ const Home = () => {
   const onClick = (x: number, y: number) => {
     console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-    //これから予測
-    for (let tate=0 ; tate<7;tate++) {
-      for (let yoko = 0 ; yoko<7;yoko++)  {
-        if (board[tate][yoko] === 0) {
-          for (let t = 0; t < directions.length; t++) {
-            console.log(Math.max(yoko + directions[t][1], 0), Math.max(tate + directions[t][0], 0));
-            if (
-              board[Math.min(Math.max(tate + directions[t][0], 0), 7)][
-                Math.max(yoko + directions[t][1], 0)
-              ] !== undefined &&
-              board[Math.min(Math.max(tate + directions[t][0], 0), 7)][
-                Math.max(yoko + directions[t][1], 0)
-              ] !== 0 &&
-              board[Math.min(Math.max(tate + directions[t][0], 0), 7)][
-                Math.max(yoko + directions[t][1], 0)
-              ] !== turnColor
-            ) {
-              if (
-                board[Math.min(Math.max(tate + directions[t][0], 0), 7)][
-                  Math.max(yoko + directions[t][1], 0)
-                ] !== turnColor
-              ) {
-                for (let p = 2; p < 8; p++) {
-                  if (
-                    board[tate + directions[t][0] * p] === undefined ||
-                    board[yoko + directions[t][1] * p] === undefined ||
-                    board[tate + directions[t][0] * p][yoko + directions[t][1] * p] === 0
-                  ) {
-                    break;
-                  }
-                  if (board[tate + directions[t][0] * p][yoko + directions[t][1] * p] === turnColor) {
-                    newBoard[tate][yoko] = 3
-                  }
-                }
-              }}
-      }
-    }
+
     //周囲一マスに違う色の駒が無いと置けない 一行目は押した場所に駒があるかどうか
     if (board[y][x] === 0) {
       for (let t = 0; t < directions.length; t++) {
@@ -118,7 +82,52 @@ const Home = () => {
 
       setBoard(newBoard);
     }
+
+    //これから予測
+    for (let tate = 0; tate < 7; tate++) {
+      for (let yoko = 0; yoko < 7; yoko++) {
+        if (board[tate][yoko] === 0) {
+          for (let t = 0; t < directions.length; t++) {
+            console.log(Math.max(yoko + directions[t][1], 0), Math.max(tate + directions[t][0], 0));
+            if (
+              board[Math.min(Math.max(tate + directions[t][0], 0), 7)][
+                Math.max(yoko + directions[t][1], 0)
+              ] !== undefined &&
+              board[Math.min(Math.max(tate + directions[t][0], 0), 7)][
+                Math.max(yoko + directions[t][1], 0)
+              ] !== 0 &&
+              board[Math.min(Math.max(tate + directions[t][0], 0), 7)][
+                Math.max(yoko + directions[t][1], 0)
+              ] !== turnColor
+            ) {
+              if (
+                board[Math.min(Math.max(tate + directions[t][0], 0), 7)][
+                  Math.max(yoko + directions[t][1], 0)
+                ] !== turnColor
+              ) {
+                for (let p = 2; p < 8; p++) {
+                  if (
+                    board[tate + directions[t][0] * p] === undefined ||
+                    board[yoko + directions[t][1] * p] === undefined ||
+                    board[tate + directions[t][0] * p][yoko + directions[t][1] * p] === 0
+                  ) {
+                    break;
+                  }
+                  if (
+                    board[tate + directions[t][0] * p][yoko + directions[t][1] * p] === turnColor
+                  ) {
+                    newBoard[tate][yoko] = 3;
+                  }
+                  setBoard(newBoard);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.board}>
@@ -128,9 +137,7 @@ const Home = () => {
               {color !== 0 && (
                 <div
                   className={styles.stone}
-                  style={{ background: color === 1 ? '#000' : (color === 3 ? '#ccff00' : '#fff') }}
-
-
+                  style={{ background: color === 1 ? '#000' : color === 3 ? '#ccff00' : '#fff' }}
                 />
               )}
             </div>
